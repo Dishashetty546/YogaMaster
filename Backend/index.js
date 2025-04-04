9b55b0f46bb08c3993bf3734c7c0ad2ad7d85175
dishashetty546 <dishashetty546@gmail.com>
2025-04-04 21:32:12 +0530

    Datbase connected - NOSQL

---
+++ Backend/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+console.log("Db user name", process.env.DB_USER);
+//dishashetty546
+//Npg0eyUum4Kj3rlQ
+
+//mongoDb connection
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yogamaster.1kpssad.mongodb.net/?retryWrites=true&w=majority&appName=YogaMaster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.listen(port, () => {
+  console.log(`Example listning on port $(port)`);
+});
